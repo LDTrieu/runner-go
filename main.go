edbefcb8b50240ed3bcedfaaa402927bb76216d1
trieuld <letrieu106@gmail.com>
2024-12-31 16:17:52 +0700

    test webhook

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/hello", helloHandler)
 	r.Run(":8081")
-	fmt.Println("Server is running on port 8084")
+	fmt.Println("Server is running on port 8080")
 }
 
 func helloHandler(c *gin.Context) {
