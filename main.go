de6de0dcfd68b74f8d73de8b90c97369496c07c6
Lê Đình Triều <51528072+LDTrieu@users.noreply.github.com>
2025-01-06 14:30:27 +0700

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/hello", helloHandler)
 	r.Run(":8081")
-	fmt.Println("Server is running on port 8085")
+	fmt.Println("Server is running on port 8083")
 }
 
 func helloHandler(c *gin.Context) {
